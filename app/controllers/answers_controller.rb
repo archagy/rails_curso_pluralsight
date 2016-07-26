ea81391a832c4b0cb3544f420860e7e3c925a515
Alex <alexiseliudrdz@gmail.com>
2016-07-26 10:45:12 -0500

    HotFix

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,10 @@
 class AnswersController < ApplicationController
 	include SuckerPunch::Job
+	
 	def create
 		question = Question.find(params[:answer][:question_id])
 		answer = question.answers.create(answer_params)
-		
+
 		MainMailer.notify_question_author(answer).deliver_later
 		session[:current_user_email] = answer_params[:email]
 		redirect_to question
